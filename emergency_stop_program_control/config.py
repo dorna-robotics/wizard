99d3bdd82a605b22f26b83624fea78b44494161e
smhty <hussein.ty@gmail.com>
2023-07-05 16:08:37 -0700

    Update config.py

--- emergency_stop_program_control/config.py
+++ emergency_stop_program_control/config.py
@@ -3,8 +3,8 @@
 ip = "localhost"
 
 # emergency stop appears when input key is equal to value
-emergency_key = None
-emergency_value = None
+emergency_key = "in0"
+emergency_value = 1
 
 # list of files to start on startup and terminate when emergency stops happens
 file_list = [
