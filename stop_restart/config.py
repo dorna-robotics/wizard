8b4cd24433072ef0638d3b120e72a6e9d670f952
smhty <hussein.ty@gmail.com>
2023-07-05 16:44:35 -0700

    stop_restrt

--- stop_restart/config.py
+++ stop_restart/config.py
@@ -12,12 +12,12 @@ file_list = [
 				"/home/dorna/Downloads/example/server_test/main.py",
 			]
 """
-# robot ip address
-ip = "localhost"
-
 # emergency stop appears when input key is equal to value
 emergency_key = None
 emergency_value = None
 
 # list of files to start on startup and terminate when emergency stops happens
 file_list = []
+
+# robot ip address
+ip = "localhost"
